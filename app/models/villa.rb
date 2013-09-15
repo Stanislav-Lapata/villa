87262a560065f538341332ee5dd02b2ca2475e6b
Oleg Haidul <oleghaidul@gmail.com>
2013-09-15 19:43:11 +0300

    Add validation

--- app/models/villa.rb
+++ app/models/villa.rb
@@ -7,4 +7,5 @@ class Villa < ActiveRecord::Base
   accepts_nested_attributes_for :images, reject_if: proc { |el| el[:image].blank? }, allow_destroy: true
 
   validates :name, presence: true
+  validates :low_night, :high_night, :peak_night, presence: true
 end
