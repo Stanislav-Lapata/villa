c5b81b1d6919b5c2de28e9412898637b9c27bdd3
Oleg Haidul <oleghaidul@gmail.com>
2013-10-20 19:48:39 +0300

    Add some assets to precompile collection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Newvilla::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.assets.precompile << 'villa.js' << 'villa.css' << 'home.css'
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
