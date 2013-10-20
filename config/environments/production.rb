408508bfab5b604c01e2840048944413ed2a0df4
Oleg Haidul <oleghaidul@gmail.com>
2013-10-20 19:51:01 +0300

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Newvilla::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  config.assets.precompile << 'villa.js' << 'villa.css' << 'home.css'
+  config.assets.precompile << 'villa.js' << 'villa.css' << 'home.js'
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
