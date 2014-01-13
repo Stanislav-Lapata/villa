6551db5482dfe3a337b7253b75552798f86000e3
Oleg Haidul <oleghaidul@gmail.com>
2014-01-13 22:24:46 +0300

    Add js to assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ JqVilla::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile << 'underscore.js' << 'gmaps/google.js' << 'gmaps_init.js'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
