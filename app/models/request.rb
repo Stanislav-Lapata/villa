3b72d12f03b9e1b8b369695249f7351812e812d5
Oleg Haidul <oleghaidul@gmail.com>
2014-01-19 21:09:44 +0300

    Fix Request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ActiveRecord::Base
   belongs_to :villa
-  validates :first_name, :last_name, :email, :check_in, :check_out, presence: true
+  validates :first_name, :last_name, :email, presence: true
 
   after_create :send_notification
 
