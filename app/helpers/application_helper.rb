0329a2bf77eeab9dd3d0ef8c9f01340fee95fc40
Oleg Haidul <oleghaidul@gmail.com>
2014-02-17 20:29:29 +0300

    Fix ru title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,44 @@ module ApplicationHelper
       "#{t('villas.price_range')}: #{number_to_currency(villa.low_night, precision: 0)} - #{number_to_currency(villa.peak_night, precision: 0)} (#{t('villas.per_night')})"
     end
   end
+
+  def site
+    if I18n.locale == :en
+      'Luxury Villas for Sale and Rent'
+    else
+      'Аренда и продажа вилл'
+    end
+  end
+
+  def rentals
+    if I18n.locale == :en
+      'Rentals'
+    else
+      'Аренда вилл на Пхукете'
+    end
+  end
+
+  def sales
+    if I18n.locale == :en
+      'Rentals'
+    else
+      'Продажа вилл на Пхукете'
+    end
+  end
+
+  def car_rentals
+    if I18n.locale == :en
+      'Cars'
+    else
+      'Аренда машин на Пхукете'
+    end
+  end
+
+  def yacht_rentals
+    if I18n.locale == :en
+      'Yachts'
+    else
+      'Аренда яхт на Пхукете'
+    end
+  end
 end
