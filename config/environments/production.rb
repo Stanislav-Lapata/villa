3e721d11a17a3cdc72d522901c3dda56e0b0d405
Oleg Haidul <oleghaidul@gmail.com>
2014-11-08 14:49:27 +0300

    Add precompilation assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,7 @@ JqVilla::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile << 'underscore.js' << 'gmaps/google.js' << 'gmaps_init.js'
-
+  config.assets.precompile += %w(underscore.js gmaps/google.js gmaps_init.js home.js destination-page villa)
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
